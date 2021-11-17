c89b9c2da9b55ab986e96a612d337cfaf6362d50
thaobone163 <71418685+thaobone163@users.noreply.github.com>
2021-11-17 14:01:44 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,9 @@ const Account = require('../models/account');
 
 const userData = {};
 
-let refreshTokens = [];
+// let refreshTokens = [];
 const accessTokenSecret = 'random';
-const refreshTokenSecret = 'refresh';
+// const refreshTokenSecret = 'refresh';
 
 router.use(bodyParser.json());
 
@@ -34,21 +34,21 @@ router.post('/login', (req, res) => {
         } else {
             if (userData.tendangnhap === tendangnhap && userData.matkhau === matkhau) {
                 // generate an access token
-                const accessToken = jwt.sign({
-                    tendangnhap: userData.tendangnhap,
-                    ID_tk: userData.ID_tk
-                }, accessTokenSecret, {expiresIn: '2000'});
-                const refreshToken = jwt.sign({
-                    tendangnhap: userData.tendangnhap,
-                    ID_tk: userData.ID_tk
-                }, refreshTokenSecret);
-
-                refreshTokens.push(refreshToken);
+                // const accessToken = jwt.sign({
+                //     tendangnhap: userData.tendangnhap,
+                //     ID_tk: userData.ID_tk
+                // }, accessTokenSecret, {expiresIn: '2000'});
+                // const refreshToken = jwt.sign({
+                //     tendangnhap: userData.tendangnhap,
+                //     ID_tk: userData.ID_tk
+                // }, refreshTokenSecret);
+                //
+                // refreshTokens.push(refreshToken);
                 console.log(userData)
 
                 res.json({
-                    accessToken,
-                    refreshToken,
+                    // accessToken,
+                    // refreshToken,
                     "ID_tk": userData.ID_tk
                 });
             } else {
@@ -59,37 +59,37 @@ router.post('/login', (req, res) => {
 
 })
 
-router.post('/token', (req, res) => {
-    const {token} = req.body;
+// router.post('/token', (req, res) => {
+//     const {token} = req.body;
+//
+//     if (!token) {
+//         return res.sendStatus(401);
+//     }
+//
+//     if (!refreshTokens.includes(token)) {
+//         return res.sendStatus(403);
+//     }
+//
+//     jwt.verify(token, refreshTokenSecret, (err, user) => {
+//         if (err) {
+//             return res.sendStatus(403);
+//         }
+//
+//         const accessToken = jwt.sign({
+//                 tendangnhap: user.tendangnhap
+//             }, accessTokenSecret,
+//             {expiresIn: '2000'});
+//
+//         res.json({
+//             accessToken
+//         });
+//     });
+// });
 
-    if (!token) {
-        return res.sendStatus(401);
-    }
-
-    if (!refreshTokens.includes(token)) {
-        return res.sendStatus(403);
-    }
-
-    jwt.verify(token, refreshTokenSecret, (err, user) => {
-        if (err) {
-            return res.sendStatus(403);
-        }
-
-        const accessToken = jwt.sign({
-                tendangnhap: user.tendangnhap
-            }, accessTokenSecret,
-            {expiresIn: '2000'});
-
-        res.json({
-            accessToken
-        });
-    });
-});
-
-router.post('/logout', (req, res) => {
-    const {tokenThis} = req.body;
-    refreshTokens = refreshTokens.filter(token => tokenThis !== token);
-    res.send("Logout successful");
-});
+// router.post('/logout', (req, res) => {
+//     const {tokenThis} = req.body;
+//     refreshTokens = refreshTokens.filter(token => tokenThis !== token);
+//     res.send("Logout successful");
+// });
 
 module.exports = router;
